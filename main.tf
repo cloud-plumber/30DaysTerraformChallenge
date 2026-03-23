6d30f6e236a08c1845ac95616f35f80b7873353d
daniel_wesh <daniel.waweru@outlook.com>
2026-03-23 22:46:11 +0300

    Added the user_data_replace_on_change argument to the aws_instance resource in main.tf to ensure that changes to the user data will trigger a replacement of the instance.

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami                    = "ami-0c94855ba95c71c99"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
