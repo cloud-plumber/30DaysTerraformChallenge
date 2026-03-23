293be0ac02ea33146059ed49e716dafff978b8ae
daniel_wesh <daniel.waweru@outlook.com>
2026-03-23 23:01:32 +0300

    Added a security group to allow connection to the web server and referenced it in the web_server instance

--- main.tf
+++ main.tf
@@ -3,10 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#------Create a security group for the web server------
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-instance-sg"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 #------EC2 Instance with simple web server------
 resource "aws_instance" "web_server" {
   ami                    = "ami-0c94855ba95c71c99"
  instance_type          = "t2.micro"
+ vpc_security_group_ids = [aws_security_group.web_sg.id]  
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
